a5e4cac7803eb2a18a1b08391768136fa9d7e55b
amcsparron2793 <amcsparron2793@users.noreply.github.com>
2024-09-06 20:59:56 -0400

    Add docstrings and refactor Runtime and WordDescrambler
    Enhanced the Runtime class with additional attributes and methods to facilitate better timing measurements. Added comprehensive docstrings to WordDescrambler and Runtime for improved code documentation and clarity. Improved verbosity control and error handling within WordDescrambler's methods.

--- WordDescrambler/Runtime.py
+++ WordDescrambler/Runtime.py
@@ -1,8 +1,60 @@
 import time
-from datetime import timedelta
+from datetime import timedelta, datetime
+
 
 class Runtime:
-    def __init__(self, start_time:float):
+    """
+    This module contains the `Runtime` class which can be used to measure the runtime of a program.
+
+    The `Runtime` class has the following attributes:
+        - `start_time`: The starting time of the program. It is a float representing the number of seconds since the epoch.
+        - `_runtime`: The calculated runtime of the program.
+        - `_runtime_seconds`: The runtime of the program in seconds.
+        - `_runtime_minutes`: The runtime of the program in minutes.
+        - `_runtime_hours`: The runtime of the program in hours.
+        - `_runtime_string`: A string representation of the runtime.
+        - `_runtime_string_suffix`: The string suffix representing the unit of measurement for the runtime.
+        - `_runtime_timedelta`: A `timedelta` object representing the runtime.
+        - `_use_timedelta`: A boolean indicating whether to use `timedelta` for representing the runtime.
+
+    The `Runtime` class has the following methods:
+        - `__init__(self, start_time:float, use_timedelta=False)`: Initializes a new `Runtime` object with the given start time and an optional flag indicating whether to use `timedelta` for representing the runtime.
+        - `__str__(self)`: Returns a string representation of the `Runtime` object.
+        - `runtime_timedelta(self)`: Calculates and returns the runtime as a `timedelta` object.
+        - `runtime_string(self)`: Generates a string representation of the runtime.
+        - `runtime(self)`: Calculates and returns the runtime in the appropriate format based on the value of `_use_timedelta`.
+        - `runtime_seconds(self)`: Calculates and returns the runtime in seconds.
+        - `runtime_minutes(self)`: Calculates and returns the runtime in minutes.
+        - `runtime_hours(self)`: Calculates and returns the runtime in hours.
+
+    Example usage:
+    ```python
+    # Create a Runtime object with a start time of 1626778400.0 seconds (July 20, 2021 12:00:00 AM)
+    rt = Runtime(1626778400.0)
+
+    # Get the string representation of the Runtime object
+    print(str(rt))  # Output: "Runtime timer: started on Tue Jul 20 00:00:00 2021."
+
+    # Get the runtime as a timedelta object
+    timedelta = rt.runtime_timedelta()
+
+    # Get the string representation of the runtime
+    string = rt.runtime_string()
+
+    # Get the runtime in the appropriate format
+    runtime = rt.runtime()
+
+    # Get the runtime in seconds
+    seconds = rt.runtime_seconds()
+
+    # Get the runtime in minutes
+    minutes = rt.runtime_minutes()
+
+    # Get the runtime in hours
+    hours = rt.runtime_hours()
+    ```
+    """
+    def __init__(self, start_time:float, use_timedelta=False):
         self.start_time = start_time
         self._runtime = None
         self._runtime_seconds = None
@@ -11,24 +63,44 @@ class Runtime:
         self._runtime_string = None
         self._runtime_string_suffix = None
         self._runtime_timedelta:timedelta or None = None
+        self._use_timedelta: bool = use_timedelta
 
     def __str__(self):
-        return self.runtime_timedelta #self.runtime_string
+        return f"Runtime timer: started on {datetime.fromtimestamp(self.start_time).ctime()}."
 
     @property
     def runtime_timedelta(self):
+        """
+        :return: The runtime of the software as a `timedelta` object.
+        """
         self._runtime_timedelta = str(timedelta(seconds=self.runtime_seconds))
         return self._runtime_timedelta
 
     @property
     def runtime_string(self):
+        """
+        :return: The runtime string representation of the program's runtime.
+        """
         self._runtime_string = f"Program runtime was {self.runtime} {self._runtime_string_suffix}."
         return self._runtime_string
 
     # TODO: have this return a dict of 'number', 'resolution' instead of using string suffix?
     @property
     def runtime(self):
-        if 60 < self.runtime_seconds < 3600:
+        """
+        Return the runtime of the object.
+
+        The method calculates and returns the runtime based on the value of the `runtime_seconds` property.
+        If the `use_timedelta` property is set to `True`, the runtime is returned as a `timedelta` object.
+        Otherwise, the runtime is returned as a numeric value representing either seconds, minutes, or hours,
+        depending on the value of `runtime_seconds`.
+
+        :return: The runtime of the object
+        """
+        if self._use_timedelta:
+            self._runtime = self.runtime_timedelta
+            self._runtime_string_suffix = 'HH:MM:SS'
+        elif 60 < self.runtime_seconds < 3600:
             self._runtime = self.runtime_minutes
             self._runtime_string_suffix = 'minutes'
         elif self.runtime_seconds > 3600:
@@ -41,12 +113,20 @@ class Runtime:
     
     @property
     def runtime_seconds(self):
+        """
+        :return: The runtime in seconds of the code block that was executed.
+        """
         end_time = time.time()
         self._runtime_seconds =  round((end_time - self.start_time), 3)
         return self._runtime_seconds
 
     @property
     def runtime_minutes(self):
+        """
+        Compute the runtime of a process in minutes.
+
+        :return: The runtime of the process in minutes.
+        """
         self._runtime_minutes = (self.runtime_seconds / 60)
         if self._runtime_minutes <= 1:
             self._runtime_minutes = round(self._runtime_minutes, 3)
@@ -56,6 +136,11 @@ class Runtime:
 
     @property
     def runtime_hours(self):
+        """
+        Calculate and return the runtime in hours.
+
+        :return: The runtime of the object in hours.
+        """
         self._runtime_hours = (self.runtime_seconds / 60 / 60)
         if self._runtime_hours <= 1:
             self._runtime_hours = round(self._runtime_hours, 3)
--- WordDescrambler/WordDescrambler.py
+++ WordDescrambler/WordDescrambler.py
@@ -9,6 +9,10 @@ from nltk.corpus import words
 
 
 def sleep_timer(total_sleep_seconds):
+    """
+    :param total_sleep_seconds: The total amount of time, in seconds, to sleep for.
+    :return: None
+    """
     counter = total_sleep_seconds
     while counter > 0:
         print(f'sleeping for {counter} more seconds')
@@ -17,13 +21,47 @@ def sleep_timer(total_sleep_seconds):
         counter -= 1
 
 class WordDescrambler:
+    """
+    WordDescrambler class
+
+    A class used to descramble words based on a given set of candidate letters.
+
+    Parameters:
+    - candidate_letters (str): The set of candidate letters used to descramble words.
+    - path_to_wordlist (Path or str, optional): The path to a wordlist file. If not provided, the default wordlist will be used. Defaults to None.
+    - **kwargs (optional): Additional keyword arguments.
+
+    Attributes:
+    - use_all_letters (bool): Whether to use all the candidate letters in a word. Defaults to False.
+    - _limit_length (int): The maximum length of the generated words. Defaults to None.
+    - _min_match_length (int): The minimum length of the matched words. Defaults to 3.
+    - verbose_mode (bool): Whether to display verbose output during the word descrambling process. Defaults to False.
+    - use_basic_wordlist (bool): Whether to use a basic wordlist. Defaults to False.
+    - _candidate_letters (list): The list of candidate letters.
+    - path_to_wordlist (Path): The path to the wordlist file.
+    - _wordlist (set): The set of words in the wordlist.
+    - match_list (set): The set of matched words.
+    - basic_wordlist (set): The set of words in the basic wordlist.
+    - full_wordlist (set): The set of words in the full wordlist.
+    - guess_counter (int): The count of guesses made during the word descrambling process.
+
+    Methods:
+    - min_match_length(): Get the minimum match length based on the number of candidate letters.
+    - limit_length(): Get the limit length for generated words.
+    - candidate_letters(): Get the list of candidate letters.
+    - Wordlist(): Get the wordlist based on the provided path or default wordlist.
+    - Wordlist(value): Set the wordlist.
+    - _run_permutations(word_length): Run permutations to generate all possible words of a given length.
+    - search(print_matches): Search for words based on the candidate letters and print the matches.
+    - print_matches(): Print the matched words.
+    """
     max_candidate_length = 12
     def __init__(self, candidate_letters: str, path_to_wordlist: Path or str = None, **kwargs):
-        # TODO: turn these into props so that they can be checked/compared.
-        #  Limit length should not be allowed to be greater than total length,
-        #  and limit length should not be allowed if use all letters is true
         self.use_all_letters = kwargs.get('use_all_letters', False)
-        self.limit_length = kwargs.get('limit_length', None)
+        self._limit_length = kwargs.get('limit_length', None)
+        self._min_match_length = kwargs.get('min_match_length', 3)
+        self.verbose_mode = kwargs.get('verbose_mode', False)
+
         self.use_basic_wordlist = kwargs.get('use_basic_wordlist', False)
 
         self._candidate_letters = [x for x in candidate_letters.lower()]
@@ -38,6 +76,24 @@ class WordDescrambler:
         self.basic_wordlist = {w.lower() for w in words.words('en-basic')}
         self.full_wordlist = {w.lower() for w in words.words()}
         self.guess_counter = 0
+
+    @property
+    def min_match_length(self):
+        if len(self.candidate_letters) >= self._min_match_length:
+            #print(len(self.candidate_letters), self._min_match_length)
+            pass
+        else:
+            raise ValueError("min_match_length must be greater than or equal to the number of candidate letters.")
+        return self._min_match_length
+
+    @property
+    def limit_length(self):
+        if self._limit_length:
+            if self._limit_length > len(self.candidate_letters):
+                raise ValueError("limit length cannot be larger than candidate_letters")
+            elif self.use_all_letters:
+                self._limit_length = None
+        return self._limit_length
 
     @property
     def candidate_letters(self):
@@ -79,7 +135,8 @@ class WordDescrambler:
             if ''.join(p) in self.Wordlist:
                 # print(f"{''.join(p)} was found in candidate letters.")
                 self.match_list.add(''.join(p))
-                print(f"found a match at guess number {self.guess_counter:,}")
+                if self.verbose_mode:
+                    print(f"found a match at guess number {self.guess_counter:,}")
 
     def search(self, print_matches: bool = False):
         if self.use_all_letters:
@@ -95,7 +152,7 @@ class WordDescrambler:
         elif self.limit_length:
             self._run_permutations(self.limit_length)
         else:
-            for r in range(3, len(self.candidate_letters)):
+            for r in range(self.min_match_length, len(self.candidate_letters)):
                 self._run_permutations(r)
 
         print(f"{len(self.match_list)} matches found.")
@@ -111,14 +168,17 @@ class WordDescrambler:
 if __name__ == '__main__':
     # TODO: GUI?
     start_time = time.time()
-    rt = Runtime(start_time)
-    #sleep_timer(5)
+    rt = Runtime(start_time, use_timedelta=True)
+    # sleep_timer(5)
 
-    WD = WordDescrambler(candidate_letters='Andrewmcspar',
+    WD = WordDescrambler(candidate_letters='AndrewMcspar',
                          use_all_letters=False, use_basic_wordlist=False)#, limit_length=5)#'stfaamnrc')
     WD.search(print_matches=True)
-    print(f"{rt}")
+
+    print(f"{rt.runtime_string}")
     with open('./Misc_Project_Files/last_runtime.txt', 'w') as f:
         f.write(str(rt))
+        f.write('\n')
+        f.write(rt.runtime_string)
 
     #print('craftsman' in [''.join(x) for x in permutations('stfaamnrc')])
